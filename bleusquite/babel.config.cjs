d581ffa4886d6244f48a6d9ca6727a9ba3795e45
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-08-13 20:08:27 -0400

    chore: Build up a resonable scaffolding before rollup/tests.

---
+++ bleusquite/babel.config.cjs
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+  presets: [
+    "@babel/preset-typescript",
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+          esmodules: false,
+        },
+        useBuiltIns: "entry",
+        corejs: "3.32.0",
+        modules: "commonjs"
+      },
+    ],
+  ],
+}
---
+++ bleusquite/src/bleusquite.ts
@@ -0,0 +1,23 @@
+import { BskyAgent, PostRecord } from "@atproto/api";
+
+export interface BleusquiteConfiguration {
+  identifier: string;
+  password: string;
+}
+
+export default class Bleusquite {
+
+  declare agent: BskyAgent;
+
+  declare postRecord: PostRecord;
+
+  post(text: string): void {
+    console.log(text);
+  }
+
+  addPhoto(location: string, alt: string){
+    console.log(location, alt);
+
+    return this;
+  }
+}
