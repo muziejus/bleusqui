6203c6403bea3de925bec3c36095c50089c256c9
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-08-16 23:05:25 -0400

    chore: Create first, crashing test.

---
+++ test-app/src/bleusquite.test.js
@@ -0,0 +1,12 @@
+import { Bleusquite } from "../../bleusquite/dist/bleusquite.js";
+import { expect } from "chai";
+
+describe("Testing Bleusquite class", function() {
+  it("can be initialized", function(done){
+    const bleusquite = new Bleusquite();
+
+    expect(bleusquite).to.be.instanceof(Bleusquite);
+
+    done();
+  })
+})
