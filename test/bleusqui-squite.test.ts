7fae0ba191e2fab3ecea064496242111af6964df
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-08-19 13:34:43 -0400

    test: Refactor addPhoto tests to handle asyncness.

--- test/bleusqui-squite.test.ts
+++ test/bleusqui-squite.test.ts
@@ -17,40 +17,57 @@ describe("Testing BleusquiSquite class", function () {
   });
 
   describe("#addPhoto()", function () {
-    it("returns itself", function () {
+    it("returns itself (as a promise)", async function () {
+      let bleusquiSquite = newBleusquiSquite();
+      bleusquiSquite = await bleusquiSquite.addPhoto("test/fixtures/cheese-puff.png", "alt text")
+      expect(bleusquiSquite).to.be.instanceof(BleusquiSquite);
+    });
+
+    it("creates postRecord.embed when the first photo is added", async function(){
+      let bleusquiSquite = newBleusquiSquite();
+      expect(bleusquiSquite.postRecord.embed).to.be.undefined;
+      bleusquiSquite = await bleusquiSquite.addPhoto("test/fixtures/cheese-puff.png", "alt text")
+      expect(bleusquiSquite.postRecord.embed).is.not.null;
+    });
+
+    it("that has an images array", async function() {
+      let bleusquiSquite = newBleusquiSquite();
+      bleusquiSquite = await bleusquiSquite.addPhoto("test/fixtures/cheese-puff.png", "alt text")
+      expect(bleusquiSquite.postRecord.embed?.images)
+        .to.be.an('array').of.length(1);
+    });
+
+    it("throws if the image is too large", async function(){
       const bleusquiSquite = newBleusquiSquite();
       expect(
-        bleusquiSquite.addPhoto("test/fixtures/cheese-puff.png", "alt text"),
-      ).to.be.instanceof(BleusquiSquite);
+        bleusquiSquite.addPhoto("test/fixtures/large-image.png", "This image is too large.")
+      ).to.be.rejectedWith(Error);
+
     });
 
-    it("creates an embed object when the first photo is added");
-
-    it("that has an images array");
-
-    it("throws if the image is too large");
-
-    it("throws when there is no alt text", function () {
+    it("throws when there is no alt text", async function () {
       const bleusquiSquite = newBleusquiSquite();
-      expect(() => {
+      expect(
         // @ts-expect-error
-        bleusquiSquite.addPhoto("test/fixtures/cheese-puff.png");
-      }).to.throw();
+        bleusquiSquite.addPhoto("test/fixtures/cheese-puff.png")
+      ).to.be.rejectedWith(Error);
     });
 
-    it("throws when a fifth photo is added", function () {
+    it("throws when a fifth photo is added", async function () {
       let bleusquiSquite = newBleusquiSquite();
 
-      [1, 2, 3, 4].forEach(() => {
-        bleusquiSquite = bleusquiSquite.addPhoto(
+      [1, 2, 3, 4].forEach(async () => {
+        bleusquiSquite = await bleusquiSquite.addPhoto(
           "test/fixtures/cheese-puff.png",
           "alt text",
         );
       });
 
-      expect(() => {
-        bleusquiSquite.addPhoto("test/fixtures/cheese-puff.png", "alt text");
-      }).to.throw();
+      expect(bleusquiSquite.postRecord.embed?.images)
+        .to.be.an('array').of.length(4);
+      expect(
+        bleusquiSquite.addPhoto("test/fixtures/cheese-puff.png", "alt text")
+      ).to.be.rejectedWith(Error);
     });
   }); // end describe #addPhoto()
 
